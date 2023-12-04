41bbb4212dfff8c4a2dfe55c85285d2346feb1ea
aliefadam <aliefadam6@gmail.com>
2023-12-04 23:03:51 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav>
+        <div class="nav-logo">
+            <a href="">GeekTutors</a>
+        </div>
+        <div class="nav-menu">
+            <a href="index.php">Home</a>
+            <a href="course.php">Course</a>
+            <a href="about.php">About</a>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
+
+    <!-- home -->
+    <section class="home">
+        <header>
+            <div class="item">
+                <div class="wrapper">
+                    <span>Welcome to GeekTutors!</span>
+                    <img src="imgs/programming 1.png" alt="">
+                </div>
+            </div>
+            <div class="item">
+                <span>Where Your Learning Dreams Come to Life with GeekTutors!</span>
+            </div>
+            <div class="item"></div>
+            <!-- <img src="imgs/home.png" alt=""> -->
+        </header>
+    </section>
+    <!-- akhir home -->
+
+    <!-- about -->
+    <section class="about">
+        <div class="item">
+            <h1>About GeekTutors</h1>
+            <p>
+                Not only can you embark on a journey of learning with GeekTutors, but you can also tailor your educational experience with our user-friendly platform. Say goodbye to standardized learning and hello to personalized and professionally curated content that will make your educational journey stand out from the rest.
+            </p>
+        </div>
+        <div class="item">
+            <img src="imgs/Rectangle 4.png" alt="">
+        </div>
+    </section>
+    <!-- akhir about -->
+
+    <!-- course -->
+    <section class="course">
+        <h1>GeekTutors Course</h1>
+        <div class="wrapper">
+            <div class="course-item">
+                <img src="imgs/Rectangle 15.png" alt="">
+                <span class="course-name">HTML</span>
+                <span class="course-desk">Become a potential and professional freelancer</span>
+            </div>
+            <div class="course-item">
+                <img src="imgs/Rectangle 15 (1).png" alt="">
+                <span class="course-name">CSS</span>
+                <span class="course-desk">Become a potential and professional freelancer</span>
+            </div>
+            <div class="course-item">
+                <img src="imgs/Rectangle 15 (2).png" alt="">
+                <span class="course-name">WEB</span>
+                <span class="course-desk">Become a potential and professional freelancer</span>
+            </div>
+        </div>
+        <button class="view-more" onclick="window.location = 'course.php'">View More</button>
+    </section>
+    <!-- akhir course -->
+
+    <!-- footer -->
+    <section class="footer">
+        <footer>
+            <div class="item">
+                <div class="sub-item">
+                    <span class="title">GeekTutors</span>
+                    <span class="sub-title">Ease of being together</span>
+                </div>
+                <div class="sub-item">
+                    <span class="title">SOCIAL MEDIA</span>
+                    <a href="" class="sub-title">Instagram</a>
+                    <a href="" class="sub-title">Twitter</a>
+                    <a href="" class="sub-title">Youtube</a>
+                </div>
+                <div class="sub-item">
+                    <span class="title">COURSE</span>
+                    <a href="" class="sub-title">HTML</a>
+                    <a href="" class="sub-title">CSS</a>
+                    <a href="" class="sub-title">WEB</a>
+                </div>
+                <div class="sub-item">
+                    <span class="title">ABOUT</span>
+                    <a href="" class="sub-title">About Us</a>
+                    <a href="" class="sub-title">Blog</a>
+                    <a href="" class="sub-title">Event</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="sub-item">
+                    <img src="imgs/Vector.png" alt="">
+                    <img src="imgs/Vector (1).png" alt="">
+                    <img src="imgs/Vector (2).png" alt="">
+                </div>
+                <div class="sub-item">
+                    <a href="">Term Of Service</a>
+                    <a href="">Privacy</a>
+                    <a href="">Return and Refund</a>
+                </div>
+                <div class="sub-item">
+                    <span>Copyright &copy; 2023</span>
+                </div>
+            </div>
+        </footer>
+    </section>
+    <!-- akhir footer -->
+</body>
+
+</html>

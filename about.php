62e3b9a6d89509b33d901189b734eded3f7f352b
aliefadam <aliefadam6@gmail.com>
2023-12-18 21:36:08 +0700

    tambah about page

---
+++ about.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/about.css">
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav>
+        <div class="nav-logo">
+            <a href="">GeekTutors</a>
+        </div>
+        <div class="nav-menu">
+            <a href="index.php">Home</a>
+            <a href="course.php">Course</a>
+            <a href="about.php">About</a>
+        </div>
+    </nav>
+    <!-- akhir navbar -->
+
+    <!-- header -->
+    <header>
+        <h1>About GeekTutors</h1>
+
+    </header>
+    <!-- akhir header -->
+
+    <section class="detail">
+        <img src="imgs/Rectangle 4.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A dolor qui eos et saepe debitis dolorem illum repellat distinctio voluptatem odio illo iure, quae ad! Magni beatae eveniet neque possimus? Nostrum pariatur id corrupti distinctio quasi beatae, quod, eligendi hic quisquam repudiandae cum! Possimus rerum nam recusandae quibusdam! At, eaque.</p>
+
+        <div class="developer">
+            <div class="row">
+                <div class="profile">
+                    <img src="imgs/alief.png" alt="">
+                    <div class="profile-detail">
+                        <span>Alief</span>
+                        <span>Front End and Back End</span>
+                    </div>
+                </div>
+                <div class="skills">
+                    <div class="atas">
+                        <div class="gambar">
+                            <img src="imgs/js-frame.png" alt="">
+                            <span>JavaScript</span>
+                        </div>
+                        <div class="gambar">
+                            <img src="imgs/php-frame.png" alt="">
+                            <span>PHP</span>
+                        </div>
+                        <div class="gambar">
+                            <img src="imgs/html-frame.png" alt="">
+                            <span>HTML</span>
+                        </div>
+                        <div class="gambar">
+                            <img src="imgs/py-frame.png" alt="">
+                            <span>Python</span>
+                        </div>
+                    </div>
+                    <div class="bawah">
+                        <span>Things Im Good At</span>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="skills">
+                    <div class="atas">
+                        <div class="gambar">
+                            <img src="imgs/figma-frame.png" alt="">
+                            <span>JavaScript</span>
+                        </div>
+                        <div class="gambar">
+                            <img src="imgs/html-frame.png" alt="">
+                            <span>PHP</span>
+                        </div>
+                        <div class="gambar">
+                            <img src="imgs/css-frame.png" alt="">
+                            <span>HTML</span>
+                        </div>
+                        <div class="gambar">
+                            <img src="imgs/ai-frame.png" alt="">
+                            <span>Python</span>
+                        </div>
+                    </div>
+                    <div class="bawah">
+                        <span>Things Im Good At</span>
+                    </div>
+                </div>
+                <div class="profile">
+                    <img src="imgs/fatih.png" alt="">
+                    <div class="profile-detail">
+                        <span>Fatih</span>
+                        <span>UI and UX</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- footer -->
+    <section class="footer">
+        <footer>
+            <div class="item">
+                <div class="sub-item">
+                    <span class="title">GeekTutors</span>
+                    <span class="sub-title">Ease of being together</span>
+                </div>
+                <div class="sub-item">
+                    <span class="title">SOCIAL MEDIA</span>
+                    <a href="" class="sub-title">Instagram</a>
+                    <a href="" class="sub-title">Twitter</a>
+                    <a href="" class="sub-title">Youtube</a>
+                </div>
+                <div class="sub-item">
+                    <span class="title">COURSE</span>
+                    <a href="" class="sub-title">HTML</a>
+                    <a href="" class="sub-title">CSS</a>
+                    <a href="" class="sub-title">WEB</a>
+                </div>
+                <div class="sub-item">
+                    <span class="title">ABOUT</span>
+                    <a href="" class="sub-title">About Us</a>
+                    <a href="" class="sub-title">Blog</a>
+                    <a href="" class="sub-title">Event</a>
+                </div>
+            </div>
+            <div class="item">
+                <div class="sub-item">
+                    <img src="imgs/Vector.png" alt="">
+                    <img src="imgs/Vector (1).png" alt="">
+                    <img src="imgs/Vector (2).png" alt="">
+                </div>
+                <div class="sub-item">
+                    <a href="">Term Of Service</a>
+                    <a href="">Privacy</a>
+                    <a href="">Return and Refund</a>
+                </div>
+                <div class="sub-item">
+                    <span>Copyright &copy; 2023</span>
+                </div>
+            </div>
+        </footer>
+    </section>
+    <!-- akhir footer -->
+</body>
+
+</html>
